797050f4aeb5c647b957a6763fef6f9524c1fe55
Giovanna Zaccolon <102630771+gzccl22@users.noreply.github.com>
2022-05-22 19:26:57 -0300

    Add files via upload

---
+++ Operadores Aritméticos/desafios/Conversor de Moedas.py
@@ -0,0 +1,3 @@
+print('Digite a quantia a ser convertida')
+N=float(input('R$:'))
+print(f'R${N} = U${N/4.88 :.2f}')
---
+++ Operadores Aritméticos/desafios/Reajuste Salarial.py
@@ -0,0 +1,3 @@
+salario=float(input('Qual é o salário do funcionário?'))
+novo=salario +(salario *15 /100)
+print('Um funcionario que ganhava R${}, com 15% de aumento, passa a receber R${}'.format(salario,novo))
