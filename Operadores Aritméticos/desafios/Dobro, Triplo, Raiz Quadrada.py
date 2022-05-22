866c35d49a9298843ac915b6383d39fc3cc0599b
Giovanna Zaccolon <102630771+gzccl22@users.noreply.github.com>
2022-05-21 21:04:20 -0300

    Add files via upload

---
+++ Operadores Aritméticos/desafios/Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,13 @@
+n =int(input('Digite um número'))
+d=n*2
+t=n*3
+r=n**(1/2)
+print('O dobro de {} vale {}'.format(n,d))
+print('O triplo de {} vale {}'.format(n,t))
+print('A raiz quadrada de {} vale {}'.format(n,r))
+
+
+#na raiz quadrada da um resultado gigante para encurtar 8.831760866327848
+#é preciso acrescentar :.2f dentro dos {}
+
+
---
+++ Operadores Aritméticos/desafios/numero sucessor e antecessor.py
@@ -0,0 +1,7 @@
+n=int(input('Digite um número:'))
+a=n-1
+s=n+1
+print('O sucessos do número {} , seu antecessor é {} e o sucessor é {}'.format(n,a,s))
+
+
+
