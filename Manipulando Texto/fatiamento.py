eea4a2bba5078388c8458b6a59bf42ab0ae9bb9d
Giovanna Zaccolon <102630771+gzccl22@users.noreply.github.com>
2022-06-12 19:55:30 -0300

    Add files via upload

---
+++ Manipulando Texto/fatiamento.py
@@ -0,0 +1,34 @@
+frase= 'Curso em vídeo Python'
+print(frase[13:])
+
+frase='Curso em video Python'
+print(frase[::7])
+
+frase= 'Curso em vídeo Python'
+print(len(frase))
+
+frase='Curso em video Python'
+print(frase.find('Vídeo'))
+
+frase= 'Curso em video Python'
+print(frase.lower().find('video'))
+
+frase='Curso em video Python'
+print(frase.split())
+
+frase='Curso em video Python'
+dividido = frase.split()
+print(dividido[0])
+
+frase='Curso em video Python'
+print(frase.upper())
+
+
+frase='Curso em video Python'
+print(frase.lower())
+
+frase='Curso em video Python'
+print(frase.capitalize())
+
+frase='Curso em video Python'
+print(frase.title())
